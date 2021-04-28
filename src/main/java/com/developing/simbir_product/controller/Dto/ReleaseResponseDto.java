93f42bbed65816d82e0ec0b3ba49bddd4d9c1678
Nikitinmn42 <nikitin42maxim@gmail.com>
2021-04-28 18:46:56 +0300

    #5: Added Release DTO

---
+++ src/main/java/com/developing/simbir_product/controller/Dto/ReleaseResponseDto.java
@@ -0,0 +1,42 @@
+package com.developing.simbir_product.controller.Dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+@Schema(description = "Релиз")
+public class ReleaseResponseDto {
+
+    @Schema(description = "Название релиза")
+    private String name;
+
+    @Schema(description = "Дата начала релиза")
+    private String startDate;
+
+    @Schema(description = "Дата конца релиза")
+    private String finishDate;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(String finishDate) {
+        this.finishDate = finishDate;
+    }
+}
