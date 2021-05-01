5690c26c96b283cb1d9dc9f4404ca6fc3e7fbcd9
Konstantin <rebrin24@gmail.com>
2021-05-01 17:59:21 +0500

    #11: added findByName method to repos

--- src/main/java/com/developing/simbir_product/repository/ProjectRepository.java
+++ src/main/java/com/developing/simbir_product/repository/ProjectRepository.java
@@ -4,8 +4,10 @@ import com.developing.simbir_product.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProjectRepository extends  JpaRepository<ProjectEntity, UUID>{
+    Optional<ProjectEntity> findByName(String name);
 }
--- src/main/java/com/developing/simbir_product/repository/TeamRepository.java
+++ src/main/java/com/developing/simbir_product/repository/TeamRepository.java
@@ -4,8 +4,10 @@ import com.developing.simbir_product.entity.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TeamRepository extends  JpaRepository<TeamEntity, UUID>{
+    Optional<TeamEntity> findByName(String name);
 }
