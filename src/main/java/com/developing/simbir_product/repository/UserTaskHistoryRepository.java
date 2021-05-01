c288f7fcb63e9e30dbabaafd1bfc2a72098c87d4
aleksandr.erenkov <al.erenkov@yandex.ru>
2021-05-01 19:31:12 +0400

    fixed userTaskHistoryRepository  => UUID

--- src/main/java/com/developing/simbir_product/repository/UserTaskHistoryRepository.java
+++ src/main/java/com/developing/simbir_product/repository/UserTaskHistoryRepository.java
@@ -4,7 +4,9 @@ import com.developing.simbir_product.entity.UserTaskHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserTaskHistoryRepository extends  JpaRepository<UserTaskHistoryEntity, UserTaskId>{
+public interface UserTaskHistoryRepository extends  JpaRepository<UserTaskHistoryEntity, UUID>{
 
 }
