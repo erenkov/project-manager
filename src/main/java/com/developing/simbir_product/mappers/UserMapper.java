f9e5a4bd0ef97593e672f453462299a8d2650a96
Nikitinmn42 <nikitin42maxim@gmail.com>
2021-05-03 13:03:49 +0300

    #23: Added UserMapper with test

---
+++ src/main/java/com/developing/simbir_product/mappers/UserMapper.java
@@ -0,0 +1,51 @@
+package com.developing.simbir_product.mappers;
+
+
+import com.developing.simbir_product.controller.Dto.UserRequestDto;
+import com.developing.simbir_product.controller.Dto.UserResponseDto;
+import com.developing.simbir_product.entity.TeamEntity;
+import com.developing.simbir_product.entity.UserEntity;
+import com.developing.simbir_product.service.TeamService;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Mapper(uses = DateTimeMapper.class, componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public abstract class UserMapper {
+
+    @Autowired
+    private TeamService teamService;
+
+
+    @Mapping(target = "fullName", source = ".")
+    @Mapping(target = "team", source = "teamId.name")
+    @Mapping(target = "email", source = "login")
+    public abstract UserResponseDto userEntityToDto(UserEntity userEntity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "login", source = "email")
+    @Mapping(target = "firstName", source = "fullName", qualifiedByName = "firstNameByFullName")
+    @Mapping(target = "lastName", source = "fullName", qualifiedByName = "lastNameByFullName")
+    @Mapping(target = "teamId", source = "team")
+    public abstract UserEntity userDtoToEntity(UserRequestDto userRequestDto);
+
+
+    public String fullNameByUser(UserEntity userEntity) {
+        return String.format("%s %s", userEntity.getFirstName(), userEntity.getFirstName());
+    }
+
+    @Named("firstNameByFullName")
+    public String firstNameByFullName(String fullName) {
+        return fullName.split(" ")[0];
+    }
+
+    @Named("lastNameByFullName")
+    public String lastNameByFullName(String fullName) {
+        return fullName.split(" ")[1];
+    }
+
+    public TeamEntity teamByName(String teamName) {
+        return teamService.findByName(teamName);
+    }
+}
---
+++ src/test/java/com/developing/simbir_product/mappers/UserMapperTest.java
@@ -0,0 +1,104 @@
+package com.developing.simbir_product.mappers;
+
+import com.developing.simbir_product.controller.Dto.UserRequestDto;
+import com.developing.simbir_product.controller.Dto.UserResponseDto;
+import com.developing.simbir_product.entity.Role;
+import com.developing.simbir_product.entity.TeamEntity;
+import com.developing.simbir_product.entity.UserEntity;
+import com.developing.simbir_product.service.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+class UserMapperTest {
+
+    UserRequestDto userRequestDto;
+    TeamEntity teamEntity;
+    UserEntity userEntity;
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    TeamService teamService;
+    @Autowired
+    TaskReleaseHistoryService taskReleaseHistoryService;
+    @Autowired
+    ReleaseService releaseService;
+    @Autowired
+    UserTaskHistoryService userTaskHistoryService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    TaskService taskService;
+    @Autowired
+    TaskMapper taskMapper;
+    @Autowired
+    ProjectService projectService;
+
+
+    @BeforeEach
+    void before() {
+        teamEntity = new TeamEntity("team name", "description");
+        teamEntity = teamService.addTeam(teamEntity);
+
+        userEntity = new UserEntity("email",
+                "password",
+                "firstname",
+                "lastname",
+                Role.USER);
+        userEntity.setUserNumber(100);
+        userEntity.setTeamId(teamEntity);
+        userEntity = userService.addUserEntity(userEntity);
+
+        userRequestDto = new UserRequestDto();
+        userRequestDto.setEmail("email");
+        userRequestDto.setFullName("FirstName LastName");
+        userRequestDto.setPassword("password");
+        userRequestDto.setTeam("team name");
+        userRequestDto.setRole("USER");
+        userRequestDto.setUserNumber(100);
+    }
+
+    @AfterEach
+    void after() {
+        userService.deleteById(userEntity.getId());
+        teamService.deleteById(teamEntity.getId());
+    }
+
+
+    @Test
+    void userEntityToDto() {
+        UserResponseDto userDtoTest = userMapper.userEntityToDto(userEntity);
+        assertEquals(userDtoTest.getUserNumber(), userEntity.getUserNumber());
+        assertEquals(userDtoTest.getEmail(), userEntity.getLogin());
+        assertEquals(userDtoTest.getFullName(), String.format("%s %s",
+                userEntity.getFirstName(),
+                userEntity.getFirstName()));
+        assertEquals(userDtoTest.getTeam(), userEntity.getTeamId().getName());
+        assertEquals(userDtoTest.getPassword(), userEntity.getPassword());
+        assertEquals(userDtoTest.getRole(), userEntity.getRole().name());
+        userEntity.setTeamId(null);
+        userDtoTest = userMapper.userEntityToDto(userEntity);
+        assertNull(userDtoTest.getTeam());
+    }
+
+    @Test
+    void userDtoToEntity() {
+        UserEntity userEntityTest = userMapper.userDtoToEntity(userRequestDto);
+        assertEquals(userEntityTest.getUserNumber(), userRequestDto.getUserNumber());
+        assertEquals(userEntityTest.getFirstName(), userRequestDto.getFullName().split(" ")[0]);
+        assertEquals(userEntityTest.getLastName(), userRequestDto.getFullName().split(" ")[1]);
+        assertEquals(userEntityTest.getPassword(), userRequestDto.getPassword());
+        assertEquals(userEntityTest.getRole().name(), userRequestDto.getRole());
+        assertEquals(userEntityTest.getLogin(), userRequestDto.getEmail());
+        assertEquals(userEntityTest.getTeamId().getName(), userRequestDto.getTeam());
+    }
+}
