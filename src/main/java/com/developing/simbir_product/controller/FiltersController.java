1b6d35f57871ce2e484ee4bd8ce1250e72e607f0
Nikitinmn42 <nikitin42maxim@gmail.com>
2021-05-07 16:28:45 +0300

    #24: Filters fixes

--- src/main/java/com/developing/simbir_product/controller/FiltersController.java
+++ src/main/java/com/developing/simbir_product/controller/FiltersController.java
@@ -5,12 +5,12 @@ import com.developing.simbir_product.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
@@ -25,14 +25,15 @@ public class FiltersController {
 
     @Operation(summary = "Получить страницу с фильтрами")
     @GetMapping
-    public ResponseEntity<String> getFilterPage() {
-        return ResponseEntity.ok("task-filters");
+    public ModelAndView getFilterPage() {
+        return new ModelAndView("task-filters", HttpStatus.OK);
     }
 
     @Operation(summary = "Преминить фильтр")
     @PostMapping
-    public ResponseEntity<String> applyFilter(TaskRequestDto taskRequestDto, Model model, Principal principal) {
-        model.addAttribute("filteredTasks", taskService.getTasksByFilter(taskRequestDto, principal));
-        return ResponseEntity.ok("task-filters");
+    public ModelAndView applyFilter(TaskRequestDto taskRequestDto, Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("task-filters", HttpStatus.OK);
+        modelAndView.addObject("filteredTasks", taskService.getTasksByFilter(taskRequestDto, principal));
+        return modelAndView;
     }
 }
