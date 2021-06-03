9662eb0ca534821cc35e60659aa3fcae04786e7f
Konstantin <rebrin24@gmail.com>
2021-06-03 17:14:39 +0500

    added YouTube params to feign client

--- src/main/java/com/developing/simbir_product/controller/YouTubeController.java
+++ src/main/java/com/developing/simbir_product/controller/YouTubeController.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-
 @RestController
 public class YouTubeController {
 
     @Autowired
     YouTubeClient youTubeClient;
 
+    String id ="7lCDEYXw3mM";
+    String key="AIzaSyAm2LbrFj0NZ7CxL2lgaGUKLtqT3jd8JNE";
+    String part="snippet,statistics";
+    String fields="items(snippet,statistics)";
+
     @GetMapping("/youtube")
-    public List<Object> getVideo() {
-        return youTubeClient.getVideo();
+    public String getVideo() {
+        return youTubeClient.getVideo(id,key,part,fields);
     }
 }
--- src/main/java/com/developing/simbir_product/feign/YouTubeClient.java
+++ src/main/java/com/developing/simbir_product/feign/YouTubeClient.java
@@ -1,18 +1,16 @@
 package com.developing.simbir_product.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "youtube-service",
-        url = "https://www.googleapis.com/youtube/v3/"
+        url = "https://www.googleapis.com/youtube/v3"
 )
 public interface YouTubeClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/videos?id=7lCDEYXw3mM&key=AIzaSyAm2LbrFj0NZ7CxL2lgaGUKLtqT3jd8JNE&part=snippet,statistics&fields=items(id,snippet,statistics)")
-    List<Object> getVideo();
+    @GetMapping("/videos")
+    String getVideo(@RequestParam("id") String id, @RequestParam("key") String key, @RequestParam("part") String part, @RequestParam("fields") String fields);
 
 }
