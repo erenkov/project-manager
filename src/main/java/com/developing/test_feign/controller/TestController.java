500a2c8cb238f38cabe4915c5b820c10a6760ab5
Konstantin <rebrin24@gmail.com>
2021-06-03 19:49:18 +0500

    minor fixes

--- src/main/java/com/developing/test_feign/controller/TestController.java
+++ src/main/java/com/developing/test_feign/controller/TestController.java
@@ -4,7 +4,6 @@ import com.developing.test_feign.Dto.RequestDto;
 import com.developing.test_feign.feign.TestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +14,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String testPostRequest() {
-        RequestDto requestDto = new RequestDto(78912, "Jason Sweet", 1, 18.00d);
-//        RequestDto requestDto = new RequestDto();
+        RequestDto requestDto = new RequestDto(57, "Ivan Ivanov", 1, 25.00d);
         return testClient.testPostRequest(requestDto);
     }
 }
